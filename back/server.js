1eae768e70aa278cf27d0fe2b3e9de5a7248bb6a
Mohamed <mookhaleddd@gmail.com>
2025-06-15 11:00:31 +0300

    fix: add .js extensions for Vercel compatibility

--- back/server.js
+++ back/server.js
@@ -1,25 +1,51 @@
+// import express from "express";
+// import "dotenv/config";
+// import coer from "cors";
+// import connectDB from "./config/db.js";
+// import { clerkMiddleware } from "@clerk/express";
+// import clerckWebhooks from "./controllers/clerkWebhooks";
+
+// connectDB();
+
+// const app = express();
+// app.use(coer());
+
+// // Middleware
+// app.use(express.json());
+// app.use(clerkMiddleware());
+
+// // API to listen to clerk webhooks
+// app.use("/api/clerk", clerckWebhooks);
+
+// app.get("/", (req, res) => res.send("API is working"));
+
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
+
+
+
+
+
+
 import express from "express";
 import "dotenv/config";
-import coer from "cors";
-import connectDB from "./config/db";
-import { clerkMiddleware } from '@clerk/express'
-import clerckWebhooks from "./controllers/clerkWebhooks";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import clerckWebhooks from "./controllers/clerkWebhooks.js";
 
 connectDB();
 
 const app = express();
-app.use(coer());
-
-
-// Middleware
-app.use(express.json())
-app.use(clerkMiddleware())
+app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware());
 
 // API to listen to clerk webhooks
-app.use("/api/clerk", clerckWebhooks)
+app.use("/api/clerk", clerckWebhooks);
 
 app.get("/", (req, res) => res.send("API is working"));
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
